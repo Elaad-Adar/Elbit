1aaacf2666971dd986014efd4b30a538ff96f163
elaad <hadar.elaad@gmail.com>
2023-01-30 16:42:40 +0200

    final groovy file for step 1

--- jenkinsfile1.groovy
+++ jenkinsfile1.groovy
@@ -4,25 +4,32 @@ pipeline {
         string(name: 'repoURL', defaultValue: 'https://github.com/Elaad-Adar/Elbit', description: 'Url of the GitHub repository')
         string(name: 'DockerHub_User', defaultValue: 'elaad5', description: 'your docker hub username')
         string(name: 'DockerHub_pwd', defaultValue: '', description: 'your docker hub password')
+        string(name: 'tag', defaultValue: 'latest', description: 'tag you image')
+    }
+    environment {
+        dockerhub = credentials('dockerhub')
     }
     stages {
         stage("Clone Repo") {
             steps {
-                git url: '${params.repoURL}'
+                script{
+                    git branch: 'main', url: repoURL
+                }
             }
         }
         stage("Build Docker Image") {
             steps {
-                sh '''
-                docker build -t ${params.DockerHub_User}/flask-docker-app:latest .
-                '''
+                script{
+                    // sh '''echo $dockerhub_PSW | docker login -u elaad5 --password-stdin'''
+                    sh 'docker login -u ${DockerHub_User} -p ${DockerHub_pwd}'
+                    sh 'docker build -t ${DockerHub_User}/flask-docker-app:${tag} .'
+                }
             }
         }
         stage("Push Docker Image") {
             steps {
                 sh '''
-                docker login --username ${params.DockerHub_User} --password ${params.DockerHub_pwd}
-                docker push ${params.DockerHub_User}/flask-docker-app:latest
+                docker push elaad5/flask-docker-app:latest
                 '''
             }
         }
